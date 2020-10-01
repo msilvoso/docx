853132ba148151cafa56ec25c5b0c2050bc01410
Manuel Silvoso <msilvoso@gmail.com>
2020-10-01 18:26:06 +0200

    Better xml cleaning

--- cleandoc.go
+++ cleandoc.go
@@ -6,28 +6,35 @@ import (
 	"strings"
 )
 
-var cleanRegex = regexp.MustCompile("<.*?>")
-
+// function to clean the docx xml
+// Shamelesly stolen from: https://github.com/PHPOffice/PHPWord
 func cleanDocXml(content string) string {
 	replacements := findPlaceholdersAndRemoveWordTags(content)
 
 	cleanContent := replacePlaceholdersByCleanVersion(replacements, content)
 
+	blocks := map[string]string{}
+	for _, s := range findContainingXmlBlockForMacro(cleanContent) {
+		blocks[s] = splitTextIntoTexts(s)
+	}
+	processedContent := replacePlaceholdersByCleanVersion(blocks, cleanContent)
 	// From: https://github.com/PHPOffice/PHPWord/issues/590
 	// $this->tempDocumentMainPart = preg_replace('/(<w:t*)>/', '$1 xml:space="preserve">', $this->tempDocumentMainPart);
-	preserveSpaces := regexp.MustCompile("(<w:t*?)>")
-	cleanContent = preserveSpaces.ReplaceAllString(cleanContent, `$1 xml:space="preserve">`)
+	//preserveSpaces := regexp.MustCompile("(<w:t*?)>")
+	//cleanContent = preserveSpaces.ReplaceAllString(cleanContent, `$1 xml:space="preserve">`)
 
-	return cleanContent
+	return processedContent
 }
 
 func findPlaceholdersAndRemoveWordTags(content string) map[string]string {
+	var cleanRegex = regexp.MustCompile("<.*?>")
+
 	replacements := make(map[string]string)
 	// matchRegexes := []string{`\{(\{|[^{]*\>)\{[^}]*\}(\}|[^{}]*\>)\}`} // find {{..}}
 	matchRegexes := []string{ // find {{..}}
-		`\}<[^{}]*?\}`, // clean between first } and second }
-		`\{<[^{}]*?\{`, // clean between first { and second {
-		`\{[^{}]*?\}`,   // clean between second { and first }
+		`}<[^{}]*?}`, // clean between first } and second }
+		`{<[^{}]*?{`, // clean between first { and second {
+		`{[^{}]*?}`,   // clean between second { and first }
 	}
 	for _, matchRegex := range matchRegexes {
 		regex := regexp.MustCompile(matchRegex)
@@ -45,4 +52,42 @@ func replacePlaceholdersByCleanVersion(replacements map[string]string, content s
 		content = strings.ReplaceAll(content, find, replace)
 	}
 	return content
+}
+
+func findContainingXmlBlockForMacro(text string) []string {
+	blocks := regexp.MustCompile(`(?i)<w:r.*?</w:r>`)
+	macros := regexp.MustCompile(`{{.*?}}`)
+	var result []string
+	for _, b := range blocks.FindAllString(text, -1) {
+		if macros.MatchString(b) {
+			result = append(result, b)
+		}
+	}
+	return result
+}
+
+func splitTextIntoTexts(text string) string {
+	if !textNeedsSplitting(text) {
+		return text
+	}
+	extractedStyles := regexp.MustCompile(`(?i)<w:rPr.*</w:rPr>`)
+	styleMatches := extractedStyles.FindAllString(text, -1)
+	var style string
+	if len(styleMatches) > 0 {
+		style = styleMatches[0]
+	}
+	nonformatted := regexp.MustCompile(`>\s+<`)
+	nonformattedText := nonformatted.ReplaceAllString(text,`><`)
+	result := strings.ReplaceAll(nonformattedText, `{{`, fmt.Sprintf("</w:t></w:r><w:r>%s<w:t xml:space=\"preserve\">{{", style))
+	result = strings.ReplaceAll(result, `}}`, fmt.Sprintf("}}</w:t></w:r><w:r>%s<w:t xml:space=\"preserve\">", style))
+	result = strings.ReplaceAll(result, fmt.Sprintf("<w:r>%s<w:t xml:space=\"preserve\"></w:t></w:r>",style), "")
+	result = strings.ReplaceAll(result, "<w:r><w:t xml:space=\"preserve\"></w:t></w:r>", "<w:t xml:space=\"preserve\">")
+	result = strings.ReplaceAll(result, "<w:t>", "")
+
+	return result
+}
+
+func textNeedsSplitting(text string) bool {
+	needsSplitting := regexp.MustCompile(`[^>]{{|}}[^<]`)
+	return needsSplitting.MatchString(text)
 }
